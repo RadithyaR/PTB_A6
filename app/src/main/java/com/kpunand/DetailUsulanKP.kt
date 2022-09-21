51b3915b93692b8aa0809f898398b62e6cbae7a0
AliaNurhikmah <2011522009_alia@student.unand.ac.id>
2022-09-22 04:12:51 +0700

    Pertama

---
+++ app/src/main/java/com/kpunand/DetailUsulanKP.kt
@@ -0,0 +1,11 @@
+package com.kpunand
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetailUsulanKP : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_usulan_kp)
+    }
+}
---
+++ app/src/main/java/com/kpunand/MahasiswaKP.kt
@@ -0,0 +1,11 @@
+package com.kpunand
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MahasiswaKP : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mahasiswa_kp)
+    }
+}
