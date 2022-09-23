ef8e901b20ffd2d9ff844c67bc285a110481e071
Radithya Romero Al Giffary <radithya.romero@gmail.com>
2022-09-23 17:07:14 +0700

    Bottom menu

---
+++ app/src/main/java/com/kpunand/ListLogbookActivity.kt
@@ -0,0 +1,13 @@
+package com.kpunand
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
+
+class ListLogbookActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list_logbook)
+    }
+    }
