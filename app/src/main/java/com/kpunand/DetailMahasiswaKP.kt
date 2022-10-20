d7fc3bb583e0de3b4be505e5689ead722234858b
Khalil 'Amir <khalilamir230701@gmail.com>
2022-10-20 14:20:54 +0700

    commit khalil

---
+++ app/src/main/java/com/kpunand/DetailMahasiswaKP.kt
@@ -0,0 +1,11 @@
+package com.kpunand
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetailMahasiswaKP : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_mahasiswa_kp)
+    }
+}
