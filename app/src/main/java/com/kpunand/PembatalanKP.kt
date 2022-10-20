3724449265845bc0d35f28d2bc42af224b559693
Khalil 'Amir <khalilamir230701@gmail.com>
2022-10-20 15:31:44 +0700

    detail mahasiswa, pembimbing kp, tambah pembimbing, seminarKP, pembatalan seminar

---
+++ app/src/main/java/com/kpunand/PembatalanKP.kt
@@ -0,0 +1,11 @@
+package com.kpunand
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PembatalanKP : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pembatalan_kp)
+    }
+}
