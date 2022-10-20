d81aa2c3035ee95b89443f62849946f260c33fac
Radithya Romero Al Giffary <radithya.romero@gmail.com>
2022-10-20 17:49:25 +0700

    list mahasiswa kp

---
+++ app/src/main/java/com/kpunand/models/MahasiswaKP.kt
@@ -0,0 +1,10 @@
+package com.kpunand.models
+
+import android.widget.ImageView
+
+data class MahasiswaKP (
+    val id: Int,
+    val gambar: ImageView?,
+    val nama: String,
+    val nim: String
+)
