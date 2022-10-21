bba017662b559bff10d3e3adde29219c9ac4c6b4
Radithya Romero Al Giffary <radithya.romero@gmail.com>
2022-10-21 14:56:07 +0700

    oke

--- app/src/main/java/com/kpunand/DetailMahasiswaKPActivity.kt
+++ app/src/main/java/com/kpunand/DetailMahasiswaKPActivity.kt
@@ -1,11 +1,26 @@
 package com.kpunand
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class DetailMahasiswaKPActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail_mahasiswa_kp)
+
+        val buttonLogbook = findViewById<Button>(R.id.logbook)
+        buttonLogbook.setOnClickListener {
+            val Intent = Intent(this, ListLogbookActivity::class.java)
+            startActivity(Intent)
+        }
+
+        val buttonSeminar = findViewById<Button>(R.id.seminarKP)
+        buttonSeminar.setOnClickListener {
+            val Intent = Intent(this, SeminarKPActivity::class.java)
+            startActivity(Intent)
+        }
+
     }
 }
