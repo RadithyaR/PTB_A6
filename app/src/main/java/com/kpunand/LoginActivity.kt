fa77a68e580fa29e8e637c87f97549ab8ae6ab5c
Radithya Romero Al Giffary <radithya.romero@gmail.com>
2022-12-07 21:10:42 +0700

    Test Notifikasi

--- app/src/main/java/com/kpunand/LoginActivity.kt
+++ app/src/main/java/com/kpunand/LoginActivity.kt
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 
-class Login : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
--- app/src/main/java/com/kpunand/MainActivity.kt
+++ app/src/main/java/com/kpunand/MainActivity.kt
@@ -4,32 +4,58 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import com.kpunand.Notifikasi.Companion.CHANNEL_ID
+
 
 class MainActivity : AppCompatActivity() {
+
+
+    private lateinit var notificationManager: NotificationManagerCompat
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val buttonMenu = findViewById<Button>(R.id.buttonMenumhs)
-        buttonMenu.setOnClickListener{
+        buttonMenu.setOnClickListener {
             val Intent = Intent(this, ListMahasiswaKPActivity::class.java)
             startActivity(Intent)
         }
         val buttonUsul = findViewById<Button>(R.id.buttonUsulan)
-        buttonUsul.setOnClickListener{
+        buttonUsul.setOnClickListener {
             val Intent = Intent(this, ListUsulanKPActivity::class.java)
             startActivity(Intent)
         }
         val buttonIns = findViewById<Button>(R.id.buttonInstansi)
-        buttonIns.setOnClickListener{
+        buttonIns.setOnClickListener {
             val Intent = Intent(this, InstansiActivity::class.java)
             startActivity(Intent)
         }
         val buttonLap = findViewById<Button>(R.id.buttonLaporan)
-        buttonLap.setOnClickListener{
+        buttonLap.setOnClickListener {
             val Intent = Intent(this, ListLaporanActivity::class.java)
             startActivity(Intent)
+        }
+
+        notificationManager = NotificationManagerCompat.from(this)
+
+        val buttonNotif = findViewById<Button>(R.id.btn_button)
+        buttonNotif.setOnClickListener {
+            val title = "Notifikasi"
+            val message = "Ini adalah pesan yang masuk"
+            val builder = NotificationCompat.Builder(this, CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_launcher_foreground)
+                .setContentTitle(title)
+                .setContentText(message)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+
+            with(NotificationManagerCompat.from(this)) {
+                // notificationId is a unique int for each notification that you must define
+                notify(1, builder.build())
+
+            }
         }
     }
 }
